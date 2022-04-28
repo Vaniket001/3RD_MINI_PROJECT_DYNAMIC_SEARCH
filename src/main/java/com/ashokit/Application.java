5d3c1c82e126190d9e42643906fcbbaca39579ef
Aniket <kumaraniket900@gmail.com>
2022-04-28 07:02:01 +0530

    second sop added in main class

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -11,6 +11,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("3RD_MINI_PROJECT_DYNAMIC_SEARCH_JRTP-19 HAS STARTED !!");
+		System.out.println("3RD_MINI_PROJECT_DYNAMIC_SEARCH_JRTP-19 HAS STARTED !!");
 	}
 
 }
